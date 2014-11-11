a4dc58c347fad4f1899ca86f7ca3dc98ae4cc34f
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2014-11-11 23:01:00 +0000

    Add diffindiff function

---
+++ rlines/R/diffindiff.R
@@ -0,0 +1,30 @@
+#' Take region names and a comparison date and does diff-in-diff
+#' 
+#' @param target.region: The region of interest, matching a backpage domain
+#' @param comparison.region.set: A set of regions (e.g. c('nova','abilene')) to compare to
+#' @param event.date: a YYYY-MM-DD date string for the actual event date
+#' @return A list with dd, main_diff, comparison_diff, pre, and post values
+
+
+
+diffindiff<-function(target.region, comparison.region.set, event.date){
+  data=twolines(target.region=target.region, comparison.region.set=comparison.region.set)
+  data$MonthDate<-as.Date(data$MonthDate, "%Y-%m-%d")
+ 
+  ed<-as.Date(event.date, "%Y-%m-%d")
+  data$post = data$MonthDate > ed
+  data<-melt(data, id=c("MonthDate","post"), variable.name="group", value.name="counts")
+  model<-lm(counts ~ post*group, data=data)
+  print(summary(model))
+  dd<-coef(model)[4] # The diff-in-diff estimate
+  target.change<-coef(model)[1]
+  #target.change<-mean(data[data$group == "target" & data$post,'counts']) - mean(data[data$group == "target" & data$post == FALSE,'counts'])
+  comparison.change<-coef(model)[1] + coef(model)[4] 
+  #comparison.change<-mean(data[data$group == "comparison" & data$post,'counts']) - mean(data[data$group == "comparison" & data$post == FALSE,'counts'])
+  
+  return(list(data=data, 
+              model=model, 
+              diff.in.diff=dd, 
+              target.diff=target.change, 
+              comparison.diff=comparison.change))
+}
