901434dd2a00a98c502a30367069f23d0e7d970b
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2014-11-17 18:08:55 +0000

    Conform with Graham's output format

--- rlines/R/quandl_test.R
+++ rlines/R/quandl_test.R
@@ -11,7 +11,14 @@ quandl_test<-function(start_date='2012-01-01', end_date=''){
     end_date = strftime(Sys.Date(),'%Y-%m-%d')
   }
   Quandl.auth('a4r7bmyM6Yqs6xqVgxZ4')
-  mytimeseries = Quandl("GOOG/NYSE_IBM", type="ts", start_date=start_date, end_date=end_date)
-  
-  return(as.data.frame(mytimeseries[,'Close']))
+  mytimeseries <- Quandl("GOOG/NYSE_IBM", type="ts", start_date=start_date, end_date=end_date)
+  df<-as.data.frame(mytimeseries[,'Close'])
+  df$DateTime<-rownames(df)
+  names(df) <- c('Value', 'DateTime')
+  return(list(
+    raw=df,
+    seasonal=df,
+    trend=df,
+    remainder=df
+    ))
 }
