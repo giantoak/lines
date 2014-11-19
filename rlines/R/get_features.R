49a7fa09d4e70359d5702a4753832c28f139df80
Jeffrey Borowitz <jeffrey.borowitz@giantoak.com>
2014-11-19 19:55:57 +0000

    Add get_features() function

---
+++ rlines/R/get_features.R
@@ -0,0 +1,19 @@
+#' Take region names and a comparison date and does diff-in-diff
+#' 
+#' @param target.region: The region of interest, matching a backpage domain
+#' @param num.matches: the number of matches to return. Defaults to 4.
+#' @return A data frame with:
+#' - matched regions as rows
+#' - features as columns
+#' - score as a final column measuring similarity
+
+get_features<-function(target.region, num.matches=4){
+  data<-region.features[,c('completeness','b01001001','counts','region')] # Select columns to use
+  data$target <- data$region == target.region # Create a binary 1 for the region we care about
+  match<-matchit(target ~ completeness + b01001001 + counts, data=data)
+  data$score <- match$distance
+  data<-data[order(data$score, decreasing=TRUE),]
+  data<-data[1:num.matches,]
+  data<-subset(data, select=-c(target))
+  return(data)
+}
